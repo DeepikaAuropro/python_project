33cb0b8dc7494307c8d91694cd3f118665adb122
DeepikaAuropro <111051092+DeepikaAuropro@users.noreply.github.com>
2022-10-13 17:28:04 +0530

    Update rev2.py

--- rev2.py
+++ rev2.py
@@ -17,6 +17,15 @@ while target_num!=guess_num:
 
 print(f'You won the game after {count} chances')
 
+
+output:
+    enter guess number3
+    The number is lesser than the target number
+    enter guess number7
+    YOU WON THE GAME
+    You won the game after 2 chances
+
+
 #------------------------------------------------------------------------------------------
 #Program to count the number of upper case and lower case letters in a string
 
@@ -34,14 +43,33 @@ def strcount():
     print(f"The lower case letters are:{len(lower_case)}")
 strcount()
 
+
+output:
+    enter stringDevOps
+    The upper case letters are:2
+    The lower case letters are:4
+
+
+
+
 #--------------------------------------------------------------------------------------------------------
 #Program to print even numbers from a list
 
 input_elements = input("Enter numbers separated by space ")
 Num_list  = input_elements.split( )
+even_list = []
 print(Num_list)
 for num in Num_list:
     value = int(num)
     if(value%2 == 0):
-        print(f'The even numbers in a list are: {value}')
+        even_list.append(value)
 
+print(f'The even numbers in a list are:  {even_list}')
+    
+    
+output:
+    Enter numbers separated by space 1 3 4 6 8 9 11 14 15 18 22 33
+    ['1', '3', '4', '6', '8', '9', '11', '14', '15', '18', '22', '33']
+    The even numbers in a list are:  [4, 6, 8, 14, 18, 22]
+
+    
